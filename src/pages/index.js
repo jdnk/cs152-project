bbac69d66ed9a20edf71bb81e7e8e55015d62c18
Jaden Kim <jadenikim@gmail.com>
2022-02-01 20:51:59 -0800

    Ahhhhhhhh.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import styled, { keyframes } from "styled-components"
+
 import {start, Start, rainbow, StyledText, Content, ContentGroup, Parent} from "../components/styles.js"
 
 const IndexPage = () => {
