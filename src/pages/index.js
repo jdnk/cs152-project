9fdf96dc255cc81ee55596f69f868798b162d7ed
Jaden Kim <jadenikim@gmail.com>
2022-02-01 20:50:00 -0800

    Bots.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import * as React from 'react'
-import Link from 'gatsby-link'
-import styled, { keyframes } from 'styled-components'
-import {start, Start, rainbow, StyledText, Content, ContentGroup, Parent} from '../components/styles.js'
+import * as React from "react"
+import * as Link from "gatsby-link"
+import styled, { keyframes } from "styled-components"
+import {start, Start, rainbow, StyledText, Content, ContentGroup, Parent} from "../components/styles.js"
 
 const IndexPage = () => {
   return (
