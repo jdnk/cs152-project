59906ad173e48d347b857990aef6122d0da9b205
Jaden Kim <jadenikim@gmail.com>
2022-02-01 20:42:44 -0800

    asdf2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from 'react'
 import Link from 'gatsby-link'
 import styled, { keyframes } from 'styled-components'
 import {start, Start, rainbow, StyledText, Content, ContentGroup, Parent} from '../components/styles.js'
